26dbbfc4d763d56efc300d99cadc88d2d0f8e7a6
pjkersha <philip.kershaw@stfc.ac.uk>
2016-09-26 16:37:34 +0100

    Fix to main func

--- ceda/tds_ogc_scan/script.py
+++ ceda/tds_ogc_scan/script.py
@@ -7,19 +7,14 @@ __copyright__ = "(C) 2016 Science and Technology Facilities Council"
 __license__ = """BSD - See LICENSE file in top-level directory"""
 __contact__ = "Philip.Kershaw@stfc.ac.uk"
 __revision__ = '$Id$'
+import sys
+import os
 import logging
 
 from ceda.tds_ogc_scan.validation import OgcTdsValidation
 
 
-def main(uri):
-    OgcTdsValidation.check(uri)
- 
-   
-if __name__ == '__main__':
-    import sys
-    import os
-
+def main():
     logging.basicConfig(level=logging.INFO)
     
     # Suppress requests logging
@@ -33,4 +28,9 @@ if __name__ == '__main__':
     else:
         uri = sys.argv[1]
 
-    main(uri)
+    OgcTdsValidation.check(uri)
+
+   
+if __name__ == '__main__':
+    main()
+        
